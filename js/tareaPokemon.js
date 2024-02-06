349040bc2ecb03c520593fcee2ac2e9b801b7196
marely <marely.carcamo@gmail.com>
2024-02-06 15:15:35 -0300

    version 2.0

--- js/tareaPokemon.js
+++ js/tareaPokemon.js
@@ -1,27 +1,25 @@
-
-async function capturaDatos(i){
-    url= "https://pokeapi.co/api/v2/pokemon/"+i;
-    let respuesta =await fetch(url);
-    let datosPokemon =await respuesta.json();
-    console.log(datosPokemon[i]);
-    urlPokemon = (datosPokemon.sprites.other.dream_world.front_default);
-    $("#img-pokemon").attr("src",urlPokemon);
-     $("h1").text(`#${datosPokemon.id} ${datosPokemon.name}`);
-}; 
- 
-function clickFlechaIzq(){
-    i = (i == 1) ? 650 : i;
-    capturaDatos(--i);
+async function capturaDatos(i) {
+	url = "https://pokeapi.co/api/v2/pokemon/" + i;
+	let respuesta = await fetch(url);
+	let datosPokemon = await respuesta.json();
+	console.log(datosPokemon[i]);
+	urlPokemon = datosPokemon.sprites.other.dream_world.front_default;
+	$("#img-pokemon").attr("src", urlPokemon);
+	$("h1").text(`#${datosPokemon.id} ${datosPokemon.name}`);
 }
 
-
-function clickFlechaDer(){
-    i = (i==649) ? 0: i;
-    capturaDatos(++i);
+function clickFlechaIzq() {
+	i = i == 1 ? 650 : i;
+	capturaDatos(--i);
 }
 
-var i=1, imagen, nombre;
-$(document).ready(function(){
-$("#flechaIzq").click(clickFlechaIzq);
-$("#flechaDer").click(clickFlechaDer);
+function clickFlechaDer() {
+	i = i == 649 ? 0 : i;
+	capturaDatos(++i);
+}
+
+var i = 1;
+	$(document).ready(function () {
+	$("#f-left").click(clickFlechaIzq);
+	$("#f-right").click(clickFlechaDer);
 });
